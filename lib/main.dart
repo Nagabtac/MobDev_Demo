3043f3e894886b7d36d1b30d395de59a7aba46c4
Nagabtac <darwin.catbagan@lorma.edu>
2026-03-03 17:55:39 +0800

    edit settings.json

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,38 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(        
-      home: Text("Welcome To Flutter Dev!"),
-    )
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("MobDev2"),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
+            padding: EdgeInsets.fromLTRB(20, 5, 20, 15),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 255, 0, 0),
+            ),
+            child: Column(
+              children: [
+                Text(
+                  "First War Kinda Nervous",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Image.asset(
+                  "assets/images/missile.jpg",
+                  width: 800,           // Set image width // Maintain aspect ratio
+                ),
+                Row(spacing:10, children: [Text("360 no scope || "),Text("Victory Dance")],)
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
   );
 }
